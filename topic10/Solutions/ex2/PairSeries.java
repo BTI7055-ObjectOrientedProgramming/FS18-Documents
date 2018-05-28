123704b5adf747fab49e26a7a7cbd4259dd15b9b
Rolf Haenni <rolf.haenni@bfh.ch>
2018-05-28 17:44:38 +0200

    Slides topic 11, solutions topic 9 and 10

---
+++ topic10/Solutions/ex2/PairSeries.java
@@ -0,0 +1,20 @@
+package topic10.ex2;
+
+import topic10.ex1.Function;
+import topic10.ex1.Predicate;
+import topic10.ex1.Series;
+
+public class PairSeries<S, T> extends Series<Pair<S, T>> {
+
+	public PairSeries(Series<S> s1, Series<T> s2) {
+		this(s1.getInitialValue(), s1.getUpdate(), s1.getCondition(), s2.getInitialValue(), s2.getUpdate(),
+				s2.getCondition());
+	}
+
+	public PairSeries(S initialValue1, Function<S> update1, Predicate<S> condition1, T initialValue2,
+			Function<T> update2, Predicate<T> condition2) {
+		super(new Pair<S, T>(initialValue1, initialValue2), new PairFunction<S, T>(update1, update2),
+				new PairPredicate<S, T>(condition1, condition2));
+	}
+	
+}
